7752998d566264ee5b92dcfd843c373cf532b2bf
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-11-19 05:51:54 +0100

    fix tests for python3

--- tests/echoprog.py
+++ tests/echoprog.py
@@ -1,5 +1,5 @@
 #echo input to stdout and twice to stderr
-
+from __future__ import print_function
 import sys
 
 input_content=sys.stdin.read()
@@ -7,6 +7,6 @@ input_content=sys.stdin.read()
 if not input_content:
     exit(42)
 
-print input_content
-print >> sys.stderr, input_content*2
+print(input_content)
+print(input_content*2, file=sys.stderr)
 exit(0)
--- tests/mockprog.py
+++ tests/mockprog.py
@@ -1,8 +1,9 @@
+from __future__ import print_function
 import sys
 
 if len(sys.argv) == 1:
     exit(42)
 
-print sys.argv[1]
-print >> sys.stderr, sys.argv[1]+sys.argv[1]
+print(sys.argv[1])
+print(sys.argv[1]+sys.argv[1], file=sys.stderr)
 exit(0)
